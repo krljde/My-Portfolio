d01122488720bad77f96da125125011f12308196
krljde <karljudemagbanlag@gmail.com>
2022-12-27 20:47:34 +0800

    Test maintenance page

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
   redirects() {
     return [
       process.env.MAINTENANCE_MODE === "1"
-        ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+        ? { source: "/((?!maintenance).*)", destination: "pages/maintenance.html", permanent: false }
         : null,
     ].filter(Boolean);
   }
