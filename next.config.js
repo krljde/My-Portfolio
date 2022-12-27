825456cc3388c729ed2d7dde37eb1349e8f1d97d
krljde <karljudemagbanlag@gmail.com>
2022-12-27 19:50:31 +0800

    Test maintenance page

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+module.exports = {
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+        : null,
+    ].filter(Boolean);
+  }
+};
+
 module.exports = nextConfig
